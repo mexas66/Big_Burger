1051399855a00b4dce8bf77793c6466a9ad6f0ae
Mehdi <mehdibenmansour.pro@gmail.com>
2020-11-30 18:30:48 +0100

    Added AdressEntity class

---
+++ src/main/java/fr/greta/java/user/persistance/UserEntity.java
@@ -0,0 +1,60 @@
+package fr.greta.java.user.persistance;
+
+import fr.greta.java.adress.domain.Adress;
+
+public class UserEntity {
+    private int id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String phone;
+    private int adress_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getAdress_id() {
+        return adress_id;
+    }
+
+    public void setAdress_id(int adress_id) {
+        this.adress_id = adress_id;
+    }
+}
