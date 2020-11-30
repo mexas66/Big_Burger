06a179584bafce2e4dce8b387d3b209d57c2e698
Mehdi <mehdibenmansour.pro@gmail.com>
2020-11-30 18:18:05 +0100

    Initial commit, added user class

---
+++ src/main/java/fr/greta/java/user/domain/User.java
@@ -0,0 +1,58 @@
+package fr.greta.java.user.domain;
+
+public class User {
+    private int id;
+    private String email;
+    private String password;
+    private String firstname;
+    private String phone;
+    private Adress adress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Adress getAdress() {
+        return adress;
+    }
+
+    public void setAdress(Adress adress) {
+        this.adress = adress;
+    }
+}
