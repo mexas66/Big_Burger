bb85a2dc7f2cac003b270d56109441e814a5bbd0
ludivine <ludivine.charbonnel@hotmail.fr>
2020-12-02 14:15:10 +0100

    add get order items method

--- src/main/java/fr/greta/java/order/persistance/OrderRepository.java
+++ src/main/java/fr/greta/java/order/persistance/OrderRepository.java
@@ -4,10 +4,13 @@ import fr.greta.java.ConnectionFactory;
 import fr.greta.java.generic.exception.RepositoryException;
 import fr.greta.java.generic.tools.JdbcTool;
 
+import java.awt.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderRepository {
 
@@ -15,6 +18,8 @@ public class OrderRepository {
 
     private static final String INSERT_INTO="INSERT INTO _order (user_id, _beginning, _end, _total)" +
             "VALUES(?, ?, ?, ?)";
+    private static final String SELECT_REQUEST_ORDER_ITEMS= "SELECT burger_id, _quantity FROM _order_items WHERE order_id =  ?";
+
 
     public OrderEntity create(OrderEntity entity) throws RepositoryException {
         Connection conn = null;
@@ -45,4 +50,36 @@ public class OrderRepository {
             JdbcTool.close(conn, statement, resultSet);
         }
     }
+
+    public List<Integer> getOrderItems(int order_id) throws RepositoryException {
+
+        List<Integer> burger_id = new ArrayList();
+
+        Connection conn = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+
+            conn = connectionFactory.create();
+            statement = conn.prepareStatement(SELECT_REQUEST_ORDER_ITEMS);
+            statement.setInt(1, order_id);
+            resultSet = statement.executeQuery();
+
+
+            while (resultSet.next()){
+
+                for(int i=0; i < resultSet.getInt("_quantity");i++){
+                    burger_id.add(resultSet.getInt("burger_id"));
+                }
+
+            }
+            return burger_id;
+        } catch(SQLException | ClassNotFoundException e){
+            throw new RepositoryException("Erreur lors de l'execution de la requete: "+SELECT_REQUEST_ORDER_ITEMS, e);
+        }finally{
+            JdbcTool.close(conn,statement,resultSet);
+        }
+    }
+
 }
